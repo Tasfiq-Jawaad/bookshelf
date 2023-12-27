75e918dabd8d12684752d6ead7eec9fde198da55
Tasfiq-Jawaad <sc23mtj@gmail.com>
2023-12-27 18:55:23 +0000

    daisyui installed and theme added

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html data-theme="light" lang="en">
+      <body>{children}</body>
     </html>
   )
 }
