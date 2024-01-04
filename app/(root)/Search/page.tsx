04f1f9d9c810eab346148b11636d7caf6f09c90d
Tasfiq-Jawaad <sc23mtj@leeds.ac.uk>
2024-01-04 11:03:12 +0000

    sort option added

--- app/(root)/Search/page.tsx
+++ app/(root)/Search/page.tsx
@@ -6,14 +6,25 @@ const page = () => {
       <div className="drawer">
         <input id="my-drawer" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
-          <div className="flex justify-between">
+          <div className="flex flex-wrap justify-between gap-4">
             <h2 className="font-bold text-2xl">Showing results for snacks</h2>
-            <label
-              htmlFor="my-drawer"
-              className="btn btn-primary drawer-button"
-            >
-              Filter
-            </label>
+            <div className="flex gap-4">
+              <select className="select select-ghost w-full max-w-xs">
+                <option disabled selected>
+                  Sort by
+                </option>
+                <option>Featured</option>
+                <option>Price low to high</option>
+                <option>Price high to low</option>
+                <option>Newest</option>
+              </select>
+              <label
+                htmlFor="my-drawer"
+                className="btn btn-primary drawer-button"
+              >
+                Filter
+              </label>
+            </div>
           </div>
           <div className="flex flex-wrap justify-center mt-5 gap-5">
             <Card />
@@ -59,119 +70,22 @@ const page = () => {
             <input id="my-drawer" type="checkbox" className="drawer-toggle" />
             <label
               htmlFor="my-drawer"
-              className="btn btn-primary drawer-button"
+              className="btn btn-primary drawer-button flex w-min ml-auto"
             >
               X
             </label>
-            <div className="collapse collapse-arrow bg-base-200">
+
+            {/* <div className="collapse collapse-arrow bg-base-200">
               <input type="checkbox" name="my-accordion-2" />
-              <div className="collapse-title text-xl font-medium">Sort by</div>
-              <div className="collapse-content">
-                <div>
-                  <input
-                    className="w-min"
-                    id="brand1"
-                    type="checkbox"
-                    name="my-accordion"
-                    value={"Brand 1"}
-                  />
-                  <label htmlFor="brand1" className="ms-2">
-                    Brand 1
-                  </label>
-                </div>
-
-                <div>
-                  <input
-                    className="w-min"
-                    id="brand1"
-                    type="checkbox"
-                    name="my-accordion"
-                    value={"Brand 1"}
-                  />
-                  <label htmlFor="brand1" className="ms-2">
-                    Brand 2
-                  </label>
-                </div>
-
-                <div>
-                  <input
-                    className="w-min"
-                    id="brand1"
-                    type="checkbox"
-                    name="my-accordion"
-                    value={"Brand 1"}
-                  />
-                  <label htmlFor="brand1" className="ms-2">
-                    Brand 3
-                  </label>
-                </div>
-
-                <div>
-                  <input
-                    className="w-min"
-                    id="brand1"
-                    type="checkbox"
-                    name="my-accordion"
-                    value={"Brand 1"}
-                  />
-                  <label htmlFor="brand1" className="ms-2">
-                    Brand 4
-                  </label>
-                </div>
-
-                <div>
-                  <input
-                    className="w-min"
-                    id="brand1"
-                    type="checkbox"
-                    name="my-accordion"
-                    value={"Brand 1"}
-                  />
-                  <label htmlFor="brand1" className="ms-2">
-                    Brand 5
-                  </label>
-                </div>
-
-                <div>
-                  <input
-                    className="w-min"
-                    id="brand1"
-                    type="checkbox"
-                    name="my-accordion"
-                    value={"Brand 1"}
-                  />
-                  <label htmlFor="brand1" className="ms-2">
-                    Brand 6
-                  </label>
-                </div>
-
-                <div>
-                  <input
-                    className="w-min"
-                    id="brand1"
-                    type="checkbox"
-                    name="my-accordion"
-                    value={"Brand 1"}
-                  />
-                  <label htmlFor="brand1" className="ms-2">
-                    Brand 7
-                  </label>
-                </div>
-
-                <div>
-                  <input
-                    className="w-min"
-                    id="brand1"
-                    type="checkbox"
-                    name="my-accordion"
-                    value={"Brand 1"}
-                  />
-                  <label htmlFor="brand1" className="ms-2">
-                    Brand 8
-                  </label>
-                </div>
-              </div>
-            </div>
+              <select className="select select-ghost w-full max-w-xs text-xl font-medium">
+                <option disabled selected>
+                  Sort by
+                </option>
+                <option>Svelte</option>
+                <option>Vue</option>
+                <option>React</option>
+              </select>
+            </div> */}
 
             {/* 2nd */}
 
